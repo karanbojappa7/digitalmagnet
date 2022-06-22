f83de95a48aac71bda9c700bd1a4ad6df1c095dc
karanbojappa7 <63574624+karanbojappa7@users.noreply.github.com>
2022-06-23 01:12:50 +0530

    .

--- works/works.php
+++ works/works.php
@@ -27,47 +27,50 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery-bez@1.0.11/src/jquery.bez.js"></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <link rel="stylesheet" href="../style_sheets/style.css">
-    
+
 </head>
 
 <body>
 
     <!-- ..start..   -->
 
-    <nav class="navbar navbar-expand-md  fixed-top nav">
-        <div class="navbar-brand col-sm-3 offset-lg-1 ">
-            <img src="../style_sheets/pictures/logo.png" width="60%" class="nodrag logoxs">
-
-            <div class="col-1 offset-10 d-block d-sm-none navtogler  navbar-dark">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <nav class="navbar ">
+        <div id="mainbox" onclick="openFunction()">&#9776;</div>
+        <div id="menu" class="sidemenu">
+            <a href="#" id="link1" id="link1" class="tsb fon1">Home</a>
+            <a href="#" id="link2" id="link2" class="tsb fon1">About</a>
+            <a href="#" id="link3" id="link3" class="tsb fon1">Services</a>
+            <a href="#" id="link4" id="link4" class="tsb fon1">Clients</a>
+            <a href="#" id="link5" id="link5" class="tsb fon1">Contact</a>
+            <div class="col-sm-10 mx-auto pt-5 m-5">
+                <img src="style_sheets/pictures/logolgwhite.png" width="100%">
             </div>
-
+            <div class="closebtn" onclick="closeFunction()">&times;</div>
         </div>
+    </nav>
 
 
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <div class="col-3 offset-md-2 offset-lg-4 offset-xl-5">
-                <ul class="navbar-nav fon1">
-                    <li class="nav-item">
-                        <div class="nav-link" id="link1">Home</div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="nav-link" id="link2">About</div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="nav-link" id="link3">Services</div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="nav-link" id="link4">Clients</div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="nav-link" id="link5">Contact</div>
-                    </li>
-                </ul>
-            </div>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <div class="col-3 offset-md-2 offset-lg-4 offset-xl-5">
+            <ul class="navbar-nav fon1">
+                <li class="nav-item">
+                    <div class="nav-link" id="link1">Home</div>
+                </li>
+                <li class="nav-item">
+                    <div class="nav-link" id="link2">About</div>
+                </li>
+                <li class="nav-item">
+                    <div class="nav-link" id="link3">Services</div>
+                </li>
+                <li class="nav-item">
+                    <div class="nav-link" id="link4">Clients</div>
+                </li>
+                <li class="nav-item">
+                    <div class="nav-link" id="link5">Contact</div>
+                </li>
+            </ul>
         </div>
+    </div>
     </nav>
 
 
@@ -82,30 +85,85 @@
                     <span class="filter-item" data-filter="Youtube">Youtube</span>
                 </div>
             </div>
-            <div class="row">           
+            <div class="row">
                 <?php 
 $db = mysqli_connect("localhost","root","","dmm");
 mysqli_select_db($db,'dmm');
 $query = "select * from yt";
 $queryform = mysqli_query($db,$query);
 if(mysqli_num_rows($queryform) > 0){
-    while($result= mysqli_fetch_array($queryform)){
-   ?>
-                
-                    <div class="gallery-item mt-2 <?php echo $result['category']?> ">
-                        <div class="card text-center bg-light">
-                            <iframe src="https://www.youtube.com/embed/<?php echo $result['links']?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                <h4 class="card-title pt-1"><?php echo $result['head'] ?></h4>
-				                <p class="card-text pb-1"><?php echo $result['description'] ?></p>
-                        </div>
+while($result= mysqli_fetch_array($queryform)){
+?>
+
+                <div class="gallery-item mt-2 <?php echo $result['category']?> ">
+                    <div class="card text-center bg-light">
+                        <iframe src="https://www.youtube.com/embed/<?php echo $result['links']?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <h4 class="card-title pt-1"><?php echo $result['head'] ?></h4>
+                        <p class="card-text pb-1"><?php echo $result['description'] ?></p>
                     </div>
-<?php
-    }
+                </div>
+                <?php
+}
 }
 ?>
             </div>
         </div>
     </section>
+
+    <footer>
+        <div class="container-fluid p-5">
+            <div class="row">
+                <div class="col-sm-4 pt-3 pb-3 order-3 order-sm-1 order-lg-1 ">
+                    <img src="../style_sheets/pictures/logowhite.png" width="100%">
+
+                    <div class="pt-2 text-center justify-content-center soc ">
+                        <a href="mailto:@gmail.com?&subject=Hello"><i class="bsw fa fa-envelope mailicon" aria-hidden="true"></i>
+                        </a>
+                        <a href="https://www.facebook.com/Digitalmagentmedia/" target="_blank"><i class="fa fa-facebook-square fbicon" aria-hidden="true"></i>
+                        </a>
+                        <a href="https://web.whatsapp.com/send?phone=+91 7259455013&text&app_absent=0" target="_blank"> <i class="fa fa-whatsapp wappicon" aria-hidden="true"></i>
+                        </a>
+                        <a href="https://www.instagram.com/digitalmagnet.media/" target="_blank"><i class="fa fa-instagram insicon" aria-hidden="true"></i>
+                        </a>
+                        <a href="" target="_blank"><i class="fa fa-youtube-play yticon" aria-hidden="true"></i>
+                        </a>
+
+                    </div>
+
+                </div>
+
+
+                <div class="col-sm-4 pt-4 text-center ulsec fon1 bgc7 order-1">
+                    <span class="bgc10 usel d-block">Usefull links:
+                    </span>
+                    <div class="fon1 ">
+                        <br>
+                        <div class="fon3 foottxt row mx-auto text-center ">
+                            <div id="flink1" class="folink">Home</div>
+                            <div id="flink2" class="folink">About</div>
+                            <div id="flink3" class="folink">Services</div>
+                            <div id="flink4" class="folink">Clients</div>
+                            <div id="flink4" class="folink">Works</div>
+                            <div id="flink5" class="folink">Contact</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 order-2">
+                    <div class="pl-5 pr-5 pt-3 pb-3 map col-sm-12">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d31104.522779869072!2d77.515258072429!3d12.96767028619905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sDigital%20Magnet%20Media%20%2COld%20Annaporneshwari%20Nagar%20Police%20station%20Building%2C%20Papreddy%20Palya%2C%202nd%20Stage%2C%20Naagarabhaavi%2C%20Bengaluru%2C%20Karnataka%20560072.!5e0!3m2!1sen!2sin!4v1653868074360!5m2!1sen!2sin" width="100%;" style="box-shadow: 0 10px 20px -20px rgba(255,255,255, 0.5); border: .01px solid var(--c10);" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+
+                </div>
+                <div class="cpy mx-auto order-last pb-2">
+
+                    <div class="zi text-center fon3 col-sm-12">
+                        ©Reach On Digital. All Rights Reserved
+                    </div>
+                    <div class="bgc5 fon1 text-center loc pt-1 ">Handcrafted by<a href="http://bbk.epizy.com/" target="_blank" class="p-2 me"><span class="me bgc5">Life_On_Code</a><span class="me heart"><img src="../style_sheets/pictures/heart.png" class="sm-1" width="15px"></span> </span></div>
+                </div>
+            </div>
+        </div>
+    </footer>
     <script>
         const filterContainer = document.querySelector(".gallery-filter"),
             galleryItems = document.querySelectorAll(".gallery-item");
