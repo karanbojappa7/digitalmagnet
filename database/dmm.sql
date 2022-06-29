97f731082315c5eb33d820204d4f4cd72f7429cf
karanbojappa7 <63574624+karanbojappa7@users.noreply.github.com>
2022-06-29 16:51:50 +0530

    .

--- database/dmm.sql
+++ database/dmm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 06, 2022 at 03:29 PM
+-- Generation Time: Jun 29, 2022 at 01:21 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -44,8 +44,48 @@ INSERT INTO `query` (`id`, `name`, `num`, `mail`, `message`) VALUES
 (2, 'karan', '9964642973', 'karanbojappa7@gmail.com', 'escn'),
 (3, 'bb', '9964642973', 'karanbojappa7@gmail.com', 'testing'),
 (4, 'karan', '9964642973', 'karanbojappa7@gmail.com', 'testing 2'),
-(5, '', '', '', ''),
-(6, '', '', '', '');
+(16, 'karan', '9964642973', 'karanbojappa7@gmail.com', 'testing 3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `user_id` int(11) NOT NULL,
+  `user_GUID` varchar(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `entry_date` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`user_id`, `user_GUID`, `email`, `password`, `entry_date`, `status`) VALUES
+(7, 'cb509842a06', 'karan@gmail.com', 'f657341142496be46f5318b3dc8e27f1', 2147483647, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user_profile`
+--
+
+CREATE TABLE `tbl_user_profile` (
+  `profile_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user_profile`
+--
+
+INSERT INTO `tbl_user_profile` (`profile_id`, `user_id`, `name`) VALUES
+(6, 7, 'karan');
 
 -- --------------------------------------------------------
 
@@ -56,23 +96,19 @@ INSERT INTO `query` (`id`, `name`, `num`, `mail`, `message`) VALUES
 CREATE TABLE `yt` (
   `id` int(11) NOT NULL,
   `links` varchar(255) NOT NULL,
-  `category` varchar(255) NOT NULL
+  `category` varchar(255) NOT NULL,
+  `head` varchar(255) NOT NULL,
+  `description` varchar(5000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `yt`
 --
 
-INSERT INTO `yt` (`id`, `links`, `category`) VALUES
-(16, 'vdy4koj9haA', 'Instagram'),
-(17, 'vdy4koj9haA', 'Facebook'),
-(19, 'yuByaTnQPQM', 'Youtube'),
-(20, 'yuByaTnQPQM', 'Youtube'),
-(21, 'k_jqs-mzqpo', 'Youtube'),
-(22, '38sxoUr6imE', 'Instagram'),
-(23, 'NqkkTaGvwNQ', 'Instagram'),
-(24, 'caiqH3F-yAw', 'Youtube'),
-(26, 'ONyrc0jHb3A', 'Facebook');
+INSERT INTO `yt` (`id`, `links`, `category`, `head`, `description`) VALUES
+(21, 'k_jqs-mzqpo', 'Youtube', 'head5', 'this is a short description of the above video'),
+(22, '38sxoUr6imE', 'Instagram', 'head6', 'this is a short description of the above video'),
+(23, 'NqkkTaGvwNQ', 'Instagram', 'head7', 'this is a short description of the above video');
 
 --
 -- Indexes for dumped tables
@@ -83,6 +119,18 @@ INSERT INTO `yt` (`id`, `links`, `category`) VALUES
 --
 ALTER TABLE `query`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `tbl_user_profile`
+--
+ALTER TABLE `tbl_user_profile`
+  ADD PRIMARY KEY (`profile_id`);
 
 --
 -- Indexes for table `yt`
@@ -98,13 +146,25 @@ ALTER TABLE `yt`
 -- AUTO_INCREMENT for table `query`
 --
 ALTER TABLE `query`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_user_profile`
+--
+ALTER TABLE `tbl_user_profile`
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `yt`
 --
 ALTER TABLE `yt`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
